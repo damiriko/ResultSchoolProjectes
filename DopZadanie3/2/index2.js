d57f03d759b3dabde5d4e90d167fa79d40dbff99
Damiriko <damiriko80@gmail.com>
2025-01-23 18:15:06 +0300

    add DopZadanie2

--- DopZadanie3/2/index2.js
+++ DopZadanie3/2/index2.js
@@ -1,9 +1,38 @@
-function startGame(heroPlayer, enemyPlayer) {
-
-    getRandomNumberInRange(0, 1);
-
+function heatEnemy() {
+    enemy.health -= 10;
 }
 
+function heatHero() {
+    hero.health -= 10;
+}
+
+function startGame(heroPlayer, enemyPlayer) {
+    let name;
+    let health;
+
+    while (heroPlayer.health > 0 && enemyPlayer.health > 0) {
+        if (getRandomNumberInRange(0, 1) === 1) {
+            console.log('strike heroPlayer ', heroPlayer);
+            heatEnemy();
+        }
+        else {
+            console.log('strike enemyPlayer ', enemyPlayer);
+            heatHero();
+        }
+    }
+
+    if (heroPlayer.health > 0) {
+        name = heroPlayer.name;
+        health = heroPlayer.health;
+    }
+    else {
+        name = enemyPlayer.name;
+        health = enemyPlayer.health;
+    }
+
+
+    alert(`${name} победил! У него осталось ${health} здоровья`)
+}
 
 function getRandomNumberInRange(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
---
+++ DopZadanie3/5/index5.js
@@ -0,0 +1,60 @@
+
+function getAdultUsers(users) {
+
+    const keys = Object.keys(users);
+    for (const key of keys) {
+        console.log(`${key}: ${users[key]}`);
+    }
+
+    /* if (users.isArray()) {
+
+        const newUsers = users.filter((item) => {
+            return
+        });
+
+    }
+    else {
+
+    }
+*/
+    return users;
+}
+
+const usersArray = [
+    { id: '34rdca3eeb7f6fgeed471198', name: 'Andrew', age: 25 },
+    { id: '76rdca3eeb7f6fgeed471100', name: 'Alexey', age: 15 },
+    { id: '12rdca3eeb7f6fgeed4711012', name: 'Egor', age: 13 },
+    { id: '32rdca3eeb7f6fgeed471101', name: 'Kate', age: 31 },
+    { id: '98rdca3eeb7f6fgeed471102', name: 'Elena', age: 18 }
+];
+
+const usersObject = {
+    '34rdca3eeb7f6fgeed471198': {
+        id: '34rdca3eeb7f6fgeed471198',
+        name: 'Andrew',
+        age: 25
+    },
+    '76rdca3eeb7f6fgeed471100': {
+        id: '76rdca3eeb7f6fgeed471100',
+        name: 'Alexey',
+        age: 15
+    },
+    '12rdca3eeb7f6fgeed4711012': {
+        id: '12rdca3eeb7f6fgeed4711012',
+        name: 'Egor',
+        age: 13
+    },
+    '32rdca3eeb7f6fgeed471101': {
+        id: '32rdca3eeb7f6fgeed471101',
+        name: 'Kate',
+        age: 31
+    },
+    '98rdca3eeb7f6fgeed471102': {
+        id: '98rdca3eeb7f6fgeed471102',
+        name: 'Elena',
+        age: 18
+    }
+};
+
+console.log('usersArray ', getAdultUsers(usersArray));
+console.log('usersObject ', getAdultUsers(usersObject));
