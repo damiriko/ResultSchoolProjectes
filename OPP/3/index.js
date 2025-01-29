904795ee588bdb511626a0f555a37552d61f726b
Damiriko <damiriko80@gmail.com>
2025-01-30 00:09:44 +0300

    OPP Zadanie3

---
+++ OPP/3/index.js
@@ -0,0 +1,34 @@
+class Dictionary {
+    constructor(name) {
+        this.name = name;
+        this.words = new Object();
+    }
+
+    add(word, description) {
+        this.words[word] = description;
+    }
+
+    remove(word) {
+        delete this.words[word];
+    }
+
+    get(word) {
+        return this.words[word];
+    }
+
+    showAllWords() {
+        console.log(this.words);
+    }
+
+}
+
+
+const dictionary = new Dictionary('Толковый словарь');
+dictionary.add('JavaScript', 'популярный язык программирования');
+dictionary.add('Веб-разработчик', 'Человек, который создает новые сервисы и сайты или поддерживает и дополняет существующие');
+
+console.log('metod get ', dictionary.get('Веб-разработчик'));
+
+dictionary.remove('JavaScript');
+dictionary.showAllWords();
+// Веб-разработчик - Человек, который создает новые сервисы и сайты или поддерживает // и дополняет существующие
