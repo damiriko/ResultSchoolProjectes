a9fc3e4374a17ed583cd390e3c7296a06be54d03
Damiriko <damiriko80@gmail.com>
2025-01-30 22:17:09 +0300

    OPP Zadanie4

--- OPP/4/index.js
+++ OPP/4/index.js
@@ -35,3 +35,38 @@ console.log('metod get ', dictionary.get('Веб-разработчик'));
 dictionary.remove('JavaScript');
 dictionary.showAllWords();
 // Веб-разработчик - Человек, который создает новые сервисы и сайты или поддерживает // и дополняет существующие
+
+
+//Задание 4
+console.log('Zadanie4-----');
+class HardWordsDictionary extends Dictionary {
+    constructor() {
+        super(name);
+    }
+
+    add(word, description) {
+        this.words[word] = {
+            word: word,
+            description: description,
+            isDifficult: true,
+        }
+    }
+
+}
+
+
+const hardWordsDictionary = new HardWordsDictionary('Сложные слова');
+
+hardWordsDictionary.add('дилетант', 'Тот, кто занимается наукой или искусством без специальной подготовки, обладая только поверхностными знаниями.');
+
+hardWordsDictionary.add('неологизм', 'Новое слово или выражение, а также новое значение старого слова.');
+
+hardWordsDictionary.add('квант', 'Неделимая часть какой-либо величины в физике.');
+
+hardWordsDictionary.remove('неологизм');
+
+hardWordsDictionary.showAllWords();
+
+// дилетант - Тот, кто занимается наукой или искусством
+// без специальной подготовки, обладая только поверхностными знаниями.
+// квант - Неделимая часть какой-либо величины в физике.
