54c4dafa867cef6fb5297c84950dec7021e51425
Damiriko <damiriko80@gmail.com>
2025-02-15 20:03:00 +0300

    add DopZadanieOPP

---
+++ OPP/Dop1/index.js
@@ -0,0 +1,21 @@
+const attacker = {
+    archer: 30,
+    footSoldier: 55,
+    cavalry: 10,
+    artillery: 3,
+}
+
+const defender = {
+    archer: 33,
+    footSoldier: 50,
+    cavalry: 40,
+    artillery: 10,
+}
+
+
+
+
+
+attacker.attack(defender); // Наши шансы равны 1/4. Необходимо укрепление! 
+attacker.attack(defender); // Наши шансы равны 2/4. Необходимо укрепление! 
+attacker.attack(defender); // Мы усилились! Мы несомненно победим! Наши шансы высоки!
---
+++ OPP/Dop2/index.js
@@ -0,0 +1,72 @@
+class Dictionary {
+    constructor(name) {
+        this.name = name;
+        this.words = new Object();
+    }
+
+    add(word, description) {
+        this.words[word] = {
+            word: word,
+            description: description
+        }
+    }
+
+    remove(word) {
+        delete this.words[word];
+    }
+
+    get(word) {
+        return this.words[word];
+    }
+
+    showAllWords() {
+        console.log(this.words);
+    }
+
+}
+
+
+const dictionary = new Dictionary('Толковый словарь');
+dictionary.add('JavaScript', 'популярный язык программирования');
+dictionary.add('Веб-разработчик', 'Человек, который создает новые сервисы и сайты или поддерживает и дополняет существующие');
+
+console.log('metod get ', dictionary.get('Веб-разработчик'));
+
+dictionary.remove('JavaScript');
+dictionary.showAllWords();
+// Веб-разработчик - Человек, который создает новые сервисы и сайты или поддерживает // и дополняет существующие
+
+
+//Задание 4
+console.log('Zadanie4-----');
+class HardWordsDictionary extends Dictionary {
+    constructor() {
+        super(name);
+    }
+
+    add(word, description) {
+        this.words[word] = {
+            word: word,
+            description: description,
+            isDifficult: true,
+        }
+    }
+
+}
+
+
+const hardWordsDictionary = new HardWordsDictionary('Сложные слова');
+
+hardWordsDictionary.add('дилетант', 'Тот, кто занимается наукой или искусством без специальной подготовки, обладая только поверхностными знаниями.');
+
+hardWordsDictionary.add('неологизм', 'Новое слово или выражение, а также новое значение старого слова.');
+
+hardWordsDictionary.add('квант', 'Неделимая часть какой-либо величины в физике.');
+
+hardWordsDictionary.remove('неологизм');
+
+hardWordsDictionary.showAllWords();
+
+// дилетант - Тот, кто занимается наукой или искусством
+// без специальной подготовки, обладая только поверхностными знаниями.
+// квант - Неделимая часть какой-либо величины в физике.
