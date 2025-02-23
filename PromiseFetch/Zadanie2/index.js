c62288fa977b34bc3fa1dab1ae2cf7acca2b8ee6
Damiriko <damiriko80@gmail.com>
2025-02-24 01:14:37 +0300

    PromiseFetch Zadanie2 result

--- PromiseFetch/Zadanie2/index.js
+++ PromiseFetch/Zadanie2/index.js
@@ -1,37 +1,30 @@
 const URL = "https://jsonplaceholder.typicode.com/users";
 
+function addUser(user) {
 
-fetch(URL)
-    .then((response) => {
-        if (!response.ok) {
-            throw new Error("Ошибка запроса");
-        }
-        return response.json();
-    })
-    .then((data) => {
-        addUser(data);
-
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-    .finally(() => {
-        const loader = document.getElementById("loader");
-        loader.style.display = "none";
-    });
-
-
-
-function addUser(users) {
-
-    for (let user of users) {
-        console.log(user);
-        const dataContainer = document.getElementById("data-container");
-        const newUser = document.createElement("li");
-        newUser.innerHTML = `<li><a href="#">${user.username}</a></li>`;
-        dataContainer.append(newUser);
-
-    }
-
+    const dataContainer = document.getElementById("data-container");
+    const newUser = document.createElement("li");
+    newUser.innerHTML = `<li><a href="#">${user.username}</a></li>`;
+    dataContainer.append(newUser);
 
 }
+
+function getUsersByIds(arrayIDUsers) {
+    const requests = arrayIDUsers.map((id) => fetch(`${URL}/${id}`));
+
+    Promise.all(requests)
+        .then(responses => {
+            const dataresults = responses.map(response => response.json());
+            return Promise.all(dataresults);
+        })
+        .then(users => {
+            console.log(users);
+            users.forEach((user) => { addUser(user) });
+        })
+        .finally(() => {
+            const loader = document.getElementById("loader");
+            loader.style.display = "none";
+        });
+}
+
+getUsersByIds([5, 6, 2, 1]);
