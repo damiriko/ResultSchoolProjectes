8e54d96b743267f0c7a785205bb4c4818558275f
Damiriko <damiriko80@gmail.com>
2025-02-24 19:38:26 +0300

    Async Away Zadanie2

---
+++ AsyncAwait/2/index.js
@@ -0,0 +1,38 @@
+const TODOS_URL = 'https://jsonplaceholder.typicode.com/todos';
+
+// const getTodosByIds = (ids) => {
+//     const requests = ids.map((id) => fetch(`${TODOS_URL}/${id}`));
+//     Promise.all(requests)
+//         .then((responses) => {
+//             const dataResults = responses.map((data) => data.json());
+//             return Promise.all(dataResults)
+//         })
+//         .then((allTasks) => {
+//             console.log(allTasks);
+//         })
+//         .catch((error) => {
+//             console.log(error);
+//         })
+// }
+
+
+const getTodosByIds = async (ids) => {
+    try {
+        const requests = ids.map((id) => fetch(`${TODOS_URL}/${id}`));
+
+        const responses = await Promise.all(requests);
+
+        const dataResults = responses.map((data) => data.json());
+        const allTasks = await Promise.all(dataResults);
+
+        console.log(allTasks);
+
+    } catch (error) {
+        console.log('Ошибка ', error);
+    }
+
+
+
+}
+
+getTodosByIds([43, 21, 55, 100, 10]);
