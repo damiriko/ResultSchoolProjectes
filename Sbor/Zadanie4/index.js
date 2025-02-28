40c9fdbedac87c46589c85797281c7e2d71ae85d
Damiriko <damiriko80@gmail.com>
2025-02-28 23:16:49 +0300

    add Build 4

---
+++ Sbor/Zadanie4/index.js
@@ -0,0 +1,12 @@
+console.log('Hello World!');
+
+const body = document.querySelector("body");
+
+const textH1 = document.createElement('h1');
+textH1.innerText = "I love JavaScript";
+body.append(textH1);
+
+const imageJS = document.createElement('img');
+imageJS.src = "/assets/img.jpg";
+imageJS.classList.add("our-image");
+body.append(imageJS);
