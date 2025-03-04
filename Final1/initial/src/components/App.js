3543202ac1b6c0488bae89dded34b2583fe8759f
Damiriko <damiriko80@gmail.com>
2025-03-04 17:23:54 +0300

    Remove node_modules from repository

--- Final1/initial/src/components/App.js
+++ Final1/initial/src/components/App.js
@@ -4,18 +4,23 @@ import { List } from './List';
 import { ListItem } from './ListItem';
 
 export class App extends Component {
+
   setup(props) {
     this.$rootElement = document.createElement('div');
     this.$rootElement.className = 'app';
 
     // ...
-    
+    this.state = {
+      total: 0,
+      donates: []
+    };
+
     const donateForm = new Form();
     this.$rootElement.appendChild(donateForm.$rootElement);
     const donateList = new List();
     this.$rootElement.appendChild(donateList.$rootElement);
   }
-  
+
   onItemCreate(amount) {
     // ...
   }
